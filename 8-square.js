e20f2a0cde503ccb11aed1e3982d301473b232d5
Alvin Kamotho <alvinkamotho9@gmail.com>
2025-07-10 11:37:08 +0300

    Add 8-square.js

---
+++ 8-square.js
@@ -0,0 +1,9 @@
+const apple = parseInt(process.argv[2]);
+
+if (isNaN(apple)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < size; i++) {
+    console.log('X'.repeat(size));
+  }
+}
