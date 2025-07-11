54550299ef45d22f3555bbe701dfc0ef993d6bb9
Alvin Kamotho <alvinkamotho9@gmail.com>
2025-07-11 10:26:33 +0300

    Correct 4 concat.js

--- 4-concat.js
+++ 4-concat.js
@@ -1,8 +1,3 @@
 const args = process.argv.slice(2);
 
-if (args.length < 2) {
-  console.log("Please provide two arguments.");
-} else {
-  const [arg1, arg2] = args;
-  console.log(`${arg1} is ${arg2}`);
-}
+console.log(args[0], "is" ,args[1]);
