8a8c9606ce56e8262da25a101c3e621180556318
Alvin Kamotho <alvinkamotho9@gmail.com>
2025-07-11 10:10:11 +0300

    Correct 8-square.js

--- 8-square.js
+++ 8-square.js
@@ -1,9 +1,9 @@
-const apple = parseInt(process.argv[2]);
+const args = process.argv.slice(2);
+const size = Number(args[0]);
 
-if (isNaN(apple)) {
-  console.log('Missing size');
+if (!Number.isInteger(size)){
+  console.log("Missing size");
 } else {
-  for (let i = 0; i < size; i++) {
-    console.log('X'.repeat(size));
-  }
+  for (let i =0; i < size; i++)
+    console.log("x".repeat(size));
 }
